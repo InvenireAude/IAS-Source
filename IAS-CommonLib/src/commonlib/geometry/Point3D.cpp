3cac5a29a294eeb7449ddecde9cb311f5e416c1f
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2019-11-05 18:06:54 +0100

    compilation warnings removed

--- IAS-CommonLib/src/commonlib/geometry/Point3D.cpp
+++ IAS-CommonLib/src/commonlib/geometry/Point3D.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-CommonLib/src/commonlib/geometry/Point3D.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,6 +34,7 @@ Point3D::Point3D(const Point3D& other){
 Point3D& Point3D::operator=(const Point3D& other){
 	this->fX=other.fX;
 	this->fY=other.fY;
+  return *this;
 }
 
 
