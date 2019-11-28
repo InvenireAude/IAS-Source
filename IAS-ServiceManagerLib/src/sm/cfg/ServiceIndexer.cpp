5d16ba2fee050cb7605997d9d640c88fcd06cfd9
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2019-11-28 22:06:13 +0100

    service manager limit from 256 to 4096

--- IAS-ServiceManagerLib/src/sm/cfg/ServiceIndexer.cpp
+++ IAS-ServiceManagerLib/src/sm/cfg/ServiceIndexer.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-ServiceManagerLib/src/sm/cfg/ServiceIndexer.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,7 +47,7 @@ void ServiceIndexer::addEntry(const String& strAttrName,
 	GrpAttrToServiceIdxListMap::Key key(strAttrName,strAttrValue);
 
 	if(!hmGrpAttrToServiceIdxList.hasKey(key)){
-		hmGrpAttrToServiceIdxList[key]=IAS_DFT_FACTORY<BitMap>::Create(256);
+		hmGrpAttrToServiceIdxList[key]=IAS_DFT_FACTORY<BitMap>::Create(4096);
 	}
 
 	hmGrpAttrToServiceIdxList.at(key)->setBit(iIdx);
