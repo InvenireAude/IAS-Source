d1f11b7c34c08c8627a32231387b9b70c0cfef97
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2019-12-20 09:00:09 +0100

    Misleading hasResult() removed.

--- IAS-LangLib/src/lang/interpreter/proc/stmt/SortNodeHandler.cpp
+++ IAS-LangLib/src/lang/interpreter/proc/stmt/SortNodeHandler.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-LangLib/src/lang/interpreter/proc/stmt/SortNodeHandler.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -70,7 +70,7 @@ void SortNodeHandler::call(const Model::Node* pNode,
 	pProgram = pCtx->getExecStore()->createOrGetExecutable(pQualifiedNameNode->getQualifiedName(),
 																     lstSignatureTypes);
 
-	if(!pProgram->getParameters()->hasResult() ||
+	if(!pProgram->isReturningResult() ||
 		pProgram->getParameters()->getResultProperty()->getType()->getTypeEnum() != DM::Type::BooleanType)
 		IAS_THROW(BadUsageException("Sort function must return a boolean result."));
 
