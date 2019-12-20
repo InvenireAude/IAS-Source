34d6d6361e609973ec5f040860d2262b4e67b377
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2019-12-20 19:43:37 +0100

    ResultHandler sets JSON.

--- IAS-QSystemLib/src/qs/workers/proc/prog/ResultHandlerForIO.cpp
+++ IAS-QSystemLib/src/qs/workers/proc/prog/ResultHandlerForIO.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-QSystemLib/src/qs/workers/proc/prog/ResultHandlerForIO.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -102,9 +102,13 @@ void ResultHandlerForIO::handleException(::org::invenireaude::qsystem::workers::
 		return;
 
 	}catch(Exception& e2){
+
+    StringStream ssData;
+	  IAS_DFT_FACTORY<DM::JSON::JSONHelper>::PtrHolder ptrJSONHelper(IAS_DFT_FACTORY<DM::JSON::JSONHelper>::Create(pDataFactory));
+	  ptrJSONHelper->save(ssData, e.getValue());
+
 		IAS_LOG(IAS::QS::LogLevel::INSTANCE.isError(),"Send to error failed, execute method"
-									" does not know what to do with this exception:"+e2.toString() +
-									DM::XML::XMLHelper::Stringify(pDataFactory,e.getValue()));
+									" does not know what to do with this exception:"+e2.toString() + ssData.str());
 	}
 
 	IAS_THROW(RollbackMeException("Program has failed or RAW mode (the routing data type)."));
