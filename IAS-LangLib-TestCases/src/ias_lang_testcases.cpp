1e5957b7df9c625906c0fc65489ef04ce0b5a198
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2019-12-27 16:03:40 +0100

    Upcast parameters TC added.

--- IAS-LangLib-TestCases/src/ias_lang_testcases.cpp
+++ IAS-LangLib-TestCases/src/ias_lang_testcases.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-LangLib-TestCases/src/ias_lang_testcases.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -90,8 +90,9 @@ int main(int argc, char* argv[]) {
 		IAS_TEST_EXE("com::examples::basic::condassign", "customer.xml");
 		IAS_TEST_EXE("com::examples::basic::condexpr", "customer.xml");
 		IAS_TEST_EXE("com::examples::basic::newfuncalls", "customer.xml");
-
 		IAS_TEST_EXE("com::examples::basic::inlinetypes", "customer.xml");
+
+    IAS_TEST_EXE("com::examples::basic::upcastargs", "customer.xml");
 
 		/**/
 
